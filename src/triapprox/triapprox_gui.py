6f4d077dab3af42c0925b3dc1355bfb71d3b93c5
Neo-513 <92048124+Neo-513@users.noreply.github.com>
2025-06-29 14:00:23 +0800

    Add files via upload

--- src/triapprox/triapprox_gui.py
+++ src/triapprox/triapprox_gui.py
@@ -1,15 +1,13 @@
 import sys
-from functools import cache
 
-import cv2
-import numpy as np
+import pyqtgraph as pg
 from PyQt6.QtGui import QIcon
 from PyQt6.QtWidgets import QApplication, QFileDialog, QMainWindow
 from shared import util
 
-from core.advancer import STAGES
-from core.runner import Runner
-from core.visualizer import Visualizer
+from core.advancer import StageConfig
+from core.visualizer import Builder, Loader, Refresher, Renderer
+from core.worker import Worker
 from ui.gui_ui import Ui_MainWindow
 
 
@@ -19,81 +17,65 @@ class Gui(QMainWindow, Ui_MainWindow):
 		self.setupUi(self)
 
 		self.setWindowIcon(QIcon(f"{util.RESOURCE}/logo"))
-		self.radioButton_monalisa.clicked.connect(lambda: self.ref_select("mona_lisa"))
-		self.radioButton_firefox.clicked.connect(lambda: self.ref_select("firefox"))
-		self.radioButton_darwin.clicked.connect(lambda: self.ref_select("darwin"))
-		self.radioButton_custom.clicked.connect(lambda: self.ref_select("custom"))
-		self.checkBox_0.clicked.connect(lambda is_visible: self.viz_toggle(is_visible, 0))
-		self.checkBox_1.clicked.connect(lambda is_visible: self.viz_toggle(is_visible, 1))
-		self.checkBox_2.clicked.connect(lambda is_visible: self.viz_toggle(is_visible, 2))
-		util.fill(self.label_ref, create=True)
-		util.fill(self.label_approx0, create=True)
-		util.fill(self.label_approx1, create=True)
-		util.fill(self.label_approx2, create=True)
-		util.buttonize(self.pushButton, self.run_toggle, f"{util.RESOURCE}/start")
+		self.radioButton_monalisa.clicked.connect(lambda: self.on_radio_clicked(self.radioButton_monalisa, "mona_lisa"))
+		self.radioButton_firefox.clicked.connect(lambda: self.on_radio_clicked(self.radioButton_firefox, "firefox"))
+		self.radioButton_darwin.clicked.connect(lambda: self.on_radio_clicked(self.radioButton_darwin, "darwin"))
+		self.radioButton_custom.clicked.connect(lambda: self.on_radio_clicked(self.radioButton_custom, "custom"))
+		util.buttonize(self.pushButton, self.on_button_clicked, f"{util.RESOURCE}/start")
+
+		plot_widget = pg.PlotWidget()
+		self.groupBox_plot.layout().addWidget(plot_widget)
+		self.trace = Builder.build_trace(plot_widget)
+		self.viewport = Builder.build_viewport(self.label_ref, self.label_approx)
 
 		self.is_running = False
-		self.visualizer = Visualizer(self.groupBox_plot, self)
-		self.runner = Runner(self.visualizer)
-		self.elapsed_timer = util.ticker(self.pushButton)
-		self.polling_timer = util.poller(15, self.runner.poll)
+		self.timer = util.ticker(self.pushButton)
+		self.worker = self.reference_pool = self.selected_radio = None
+		self.radioButton_monalisa.click()
 
-	def ref_select(self, name):
-		util.fill(self.label_ref)
-		util.fill(self.label_approx0)
-		util.fill(self.label_approx1)
-		util.fill(self.label_approx2)
-
-		path = QFileDialog.getOpenFileName(filter="*.png")[0] if name == "custom" else f"{util.RESOURCE}/{name}.png"
-		if path:
-			self.visualizer.reference_pool = self.ref_load(path)
-			self.visualizer.render(self.label_ref)
+	def on_radio_clicked(self, radio, ref_name):
+		if ref_name == "custom":
+			ref_path = QFileDialog.getOpenFileName(filter="*.png")[0]
 		else:
-			self.visualizer.reference_pool = None
+			ref_path = f"{util.RESOURCE}/{ref_name}.png"
 
-	@staticmethod
-	@cache
-	def ref_load(path, dtype=np.uint8):
-		reference = cv2.cvtColor(cv2.imread(path), cv2.COLOR_BGR2RGB)
-		reference_pool = {}
-		for stage in STAGES:
-			resized_reference = cv2.resize(reference, (stage.resolution, stage.resolution))
-			blurred_reference = cv2.GaussianBlur(resized_reference, (stage.blur_kernel_size, stage.blur_kernel_size), 0)
-			reference_pool[stage.resolution] = blurred_reference.astype(dtype)
-		return reference_pool
+		if ref_path:
+			self.reference_pool = Loader.load_reference_pool(ref_path)
+			Renderer.render_ref(self.viewport, self.reference_pool[StageConfig.FINAL_RESOLUTION])
+			self.selected_radio = radio
+		else:
+			self.selected_radio.click()
 
-	def run_toggle(self):
-		if not self.visualizer.reference_pool:
-			util.popup("请选择图片", f"{util.RESOURCE}/error")
-			return
-
+	def on_button_clicked(self):
 		if self.is_running:
-			self.run_stop()
+			self.stop_optimization()
 		else:
-			self.run_start()
+			self.start_optimization()
 
-	def run_start(self):
+	def start_optimization(self):
 		self.is_running = True
-		self.frame_radio.setEnabled(False)
-		self.visualizer.clear()
-		self.runner.spawn()
+		self.frame.setEnabled(False)
+
+		self.trace.iterations.clear()
+		self.trace.metrics.clear()
+		self.trace.curve.clear()
+
+		self.worker = Worker(self.reference_pool)
+		self.worker.snapshot_ready.connect(lambda snapshot: Refresher.refresh_plot(self.trace, snapshot))
+		self.worker.snapshot_ready.connect(lambda snapshot: Refresher.refresh_canvas(self.viewport, snapshot))
+		self.worker.start()
+
 		self.pushButton.setIcon(QIcon(f"{util.RESOURCE}/stop"))
 		self.pushButton.setText("00:00:00")
-		self.elapsed_timer.second = 0
-		self.elapsed_timer.start()
-		self.polling_timer.start()
+		self.timer.second = 0
+		self.timer.start()
 
-	def run_stop(self):
+	def stop_optimization(self):
 		self.is_running = False
-		self.polling_timer.stop()
-		self.elapsed_timer.stop()
+		self.timer.stop()
 		self.pushButton.setIcon(QIcon(f"{util.RESOURCE}/start"))
-		self.runner.shutdown()
-		self.frame_radio.setEnabled(True)
-
-	def viz_toggle(self, is_visible, trace_id):
-		self.visualizer.traces[trace_id].tracer.chart_curve.setVisible(is_visible)
-		self.visualizer.traces[trace_id].tracer.chart_cursor.setVisible(is_visible)
+		self.worker.shutdown()
+		self.frame.setEnabled(True)
 
 
 if __name__ == "__main__":
